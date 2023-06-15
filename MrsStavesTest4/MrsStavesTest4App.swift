56e136efe9e3aaefc1b62f7575511655257ec27b
Mrs Staves <estaves@st-francis.herts.sch.uk>
2023-06-15 10:49:35 +0100

    Initial Commit

---
+++ MrsStavesTest4/MrsStavesTest4App.swift
@@ -0,0 +1,17 @@
+//
+//  MrsStavesTest4App.swift
+//  MrsStavesTest4
+//
+//  Created by SFCMacMini1 on 15/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct MrsStavesTest4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
